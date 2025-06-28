d6361913f68454f5ef5cbb69f8843553efcc4264
haritha022004 <23211a6744@bvrit.ac.in>
2025-06-28 14:01:51 +0630

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,11 +1,9 @@
-import java.util.scanner;
+
 public class AddNumbers{
   public static void main(String[]args){
-    Scanner sc=new Scanner(System.in);
-    System.out.print("Enter first number:");
-    int num1=sc.nextInt();
-    System.out.print("Enter second number:");
-    int num2=sc.nextInt();
+   int num1=10;
+   int num2=20;
+    
     int sum=num1+num2;
     System.out.print("Sum="+sum);
   }
